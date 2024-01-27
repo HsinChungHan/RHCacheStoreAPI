471578eba3446a590567dda58be440db575b937e
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-01-28 01:27:58 +0800

    Add `RHCacheStoreAPIImplementationFactoryProtocol` as public interface

---
+++ Sources/RHCacheStoreAPI/PublicInterface/RHCacheStoreAPIImplementationFactoryProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  RHCacheStoreAPIImplementationFactoryProtocol.swift
+//
+//
+//  Created by Chung Han Hsin on 2024/1/28.
+//
+
+import Foundation
+
+public protocol RHCacheStoreAPIImplementationFactoryProtocol {
+    func makeCodableStore(with storeURL: URL, expiryTimeInterval: TimeInterval?) -> RHCacheStoreAPIProtocol
+}
+
