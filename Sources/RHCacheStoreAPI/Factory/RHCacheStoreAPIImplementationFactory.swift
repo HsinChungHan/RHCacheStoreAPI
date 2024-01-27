e19e79db3e6fb114ab521c0f664c1f0dbb352e85
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-01-28 01:28:21 +0800

    Make `RHCacheStoreAPIImplementationFactory` to conform `RHCacheStoreAPIImplementationFactoryProtocol`

---
+++ Sources/RHCacheStoreAPI/Factory/RHCacheStoreAPIImplementationFactory.swift
@@ -0,0 +1,15 @@
+//
+//  RHCacheStoreAPIImplementationFactory.swift
+//
+//
+//  Created by Chung Han Hsin on 2024/1/28.
+//
+
+import Foundation
+import RHCacheStore
+
+public struct RHCacheStoreAPIImplementationFactory: RHCacheStoreAPIImplementationFactoryProtocol  {
+    public func makeCodableStore(with storeURL: URL, expiryTimeInterval: TimeInterval?) -> RHCacheStoreAPIProtocol {
+        return CodableStoreImplementation.init(storeURL: storeURL, expiryTimeInterval: expiryTimeInterval)
+    }
+}
